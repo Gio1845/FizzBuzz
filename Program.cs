f8541768d479ff0a9d773e34347c18737504b024
Gio1845 <victor-reyesr@hotmail.com>
2020-02-12 09:17:26 -0700

    FizZbUZZ implementacion

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //primera - inicializacion
+            //antes de comenzar el for
+
+            //segunda - condicion
+            //antes de comenzar cada interacion del for
+
+            //tercera - ???
+            //despues de cada interacion
+            
+
+            
+            //while (true) {
+
+            //}
+            for (int i = 1; i <= 100; i++) {
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                } else if(i % 3 ==0){
+                   Console.WriteLine("Fizz"); 
+                }else if (i % 5 ==0){
+                    Console.WriteLine("Buzz");
+                }else{
+                    Console.WriteLine(i);
+                }
+               
+            }
+        }
+    }
+}
